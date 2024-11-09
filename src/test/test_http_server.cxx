9a651bc1e2718f429feee7f20377e81fdb3dcd5f
Carlos <carlos@Carloss-MacBook-Air.local>
2024-11-09 14:14:22 +0100

    test: add more unit tests

--- src/test/test_http_server.cxx
+++ src/test/test_http_server.cxx
@@ -121,10 +121,280 @@ TEST (HttpServer, test_post_simple_text) {
     response.status(200).send ("Hello World!");
   });
 
-  const auto [ statusFileName, bodyFileName ] = createTempFiles ();
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
 
   const auto exit = std::system (fmt::format(
     "curl -s -X POST 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -d '{}' -w '%{{http_code}}' -o {} > {}",
+    body,
+    bodyFileName.string(),
+    statusFileName.string()
+  ).c_str());
+  ASSERT_EQ (exit, 0);
+
+  const auto [ resStatus, resBody ] = readResponse (statusFileName, bodyFileName);
+  ASSERT_EQ (resStatus, 200);
+  ASSERT_EQ (resBody, "Hello World!");
+}
+
+// ----------------------------------------------------------------------------
+// test_put_simple_text
+// ----------------------------------------------------------------------------
+TEST (HttpServer, test_put_simple_text) {
+  lightning::HttpServer server { 8080, getLogLevel() };
+
+  const std::string body { "aa=bb" };
+
+  server.addRoute (lightning::HttpMethod::kPut, "/v1/hello", [ &body ] (const auto &request, auto &response) {
+    ASSERT_EQ (request.method, lightning::HttpMethod::kPut);
+    ASSERT_EQ (request.path, "/v1/hello");
+    ASSERT_EQ (request.query, "p=1");
+    ASSERT_EQ (request.url, "/v1/hello?p=1");
+    ASSERT_EQ (request.version.major, 1);
+    ASSERT_EQ (request.version.minor, 1);
+    ASSERT_EQ (request.host, "localhost");
+    ASSERT_EQ (request.ip, "127.0.0.1");
+    ASSERT_EQ (request.protocol, lightning::ProtocolType::kHttp);
+    ASSERT_TRUE (request.headers.contains("host"));
+    ASSERT_TRUE (request.headers.contains("headername"));
+    ASSERT_EQ (request.body.size(), body.size());
+    response.status(200).send ("Hello World!");
+  });
+
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
+
+  const auto exit = std::system (fmt::format(
+    "curl -s -X PUT 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -d '{}' -w '%{{http_code}}' -o {} > {}",
+    body,
+    bodyFileName.string(),
+    statusFileName.string()
+  ).c_str());
+  ASSERT_EQ (exit, 0);
+
+  const auto [ resStatus, resBody ] = readResponse (statusFileName, bodyFileName);
+  ASSERT_EQ (resStatus, 200);
+  ASSERT_EQ (resBody, "Hello World!");
+}
+
+// ----------------------------------------------------------------------------
+// test_head_simple_text
+// ----------------------------------------------------------------------------
+TEST (HttpServer, test_head_simple_text) {
+  lightning::HttpServer server { 8080, getLogLevel() };
+
+  const std::string body { "aa=bb" };
+
+  server.addRoute (lightning::HttpMethod::kHead, "/v1/hello", [ &body ] (const auto &request, auto &response) {
+    ASSERT_EQ (request.method, lightning::HttpMethod::kHead);
+    ASSERT_EQ (request.path, "/v1/hello");
+    ASSERT_EQ (request.query, "p=1");
+    ASSERT_EQ (request.url, "/v1/hello?p=1");
+    ASSERT_EQ (request.version.major, 1);
+    ASSERT_EQ (request.version.minor, 1);
+    ASSERT_EQ (request.host, "localhost");
+    ASSERT_EQ (request.ip, "127.0.0.1");
+    ASSERT_EQ (request.protocol, lightning::ProtocolType::kHttp);
+    ASSERT_TRUE (request.headers.contains("host"));
+    ASSERT_TRUE (request.headers.contains("headername"));
+    ASSERT_EQ (request.body.size(), body.size());
+    response.status(200).send ("Hello World!");
+  });
+
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
+
+  const auto exit = std::system (fmt::format(
+    "curl -s -X HEAD 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -d '{}' -w '%{{http_code}}' -o {} > {}",
+    body,
+    bodyFileName.string(),
+    statusFileName.string()
+  ).c_str());
+  ASSERT_EQ (exit, 0);
+
+  const auto [ resStatus, resBody ] = readResponse (statusFileName, bodyFileName);
+  ASSERT_EQ (resStatus, 200);
+  ASSERT_EQ (resBody, "Hello World!");
+}
+
+// ----------------------------------------------------------------------------
+// test_delete_simple_text
+// ----------------------------------------------------------------------------
+TEST (HttpServer, test_delete_simple_text) {
+  lightning::HttpServer server { 8080, getLogLevel() };
+
+  const std::string body { "aa=bb" };
+
+  server.addRoute (lightning::HttpMethod::kDelete, "/v1/hello", [ &body ] (const auto &request, auto &response) {
+    ASSERT_EQ (request.method, lightning::HttpMethod::kDelete);
+    ASSERT_EQ (request.path, "/v1/hello");
+    ASSERT_EQ (request.query, "p=1");
+    ASSERT_EQ (request.url, "/v1/hello?p=1");
+    ASSERT_EQ (request.version.major, 1);
+    ASSERT_EQ (request.version.minor, 1);
+    ASSERT_EQ (request.host, "localhost");
+    ASSERT_EQ (request.ip, "127.0.0.1");
+    ASSERT_EQ (request.protocol, lightning::ProtocolType::kHttp);
+    ASSERT_TRUE (request.headers.contains("host"));
+    ASSERT_TRUE (request.headers.contains("headername"));
+    ASSERT_EQ (request.body.size(), body.size());
+    response.status(200).send ("Hello World!");
+  });
+
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
+
+  const auto exit = std::system (fmt::format(
+    "curl -s -X DELETE 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -d '{}' -w '%{{http_code}}' -o {} > {}",
+    body,
+    bodyFileName.string(),
+    statusFileName.string()
+  ).c_str());
+  ASSERT_EQ (exit, 0);
+
+  const auto [ resStatus, resBody ] = readResponse (statusFileName, bodyFileName);
+  ASSERT_EQ (resStatus, 200);
+  ASSERT_EQ (resBody, "Hello World!");
+}
+
+// ----------------------------------------------------------------------------
+// test_connect_simple_text
+// ----------------------------------------------------------------------------
+TEST (HttpServer, test_connect_simple_text) {
+  lightning::HttpServer server { 8080, getLogLevel() };
+
+  server.addRoute (lightning::HttpMethod::kConnect, "/v1/hello", [] (const auto &request, auto &response) {
+    ASSERT_EQ (request.method, lightning::HttpMethod::kConnect);
+    ASSERT_EQ (request.path, "/v1/hello");
+    ASSERT_EQ (request.query, "p=1");
+    ASSERT_EQ (request.url, "/v1/hello?p=1");
+    ASSERT_EQ (request.version.major, 1);
+    ASSERT_EQ (request.version.minor, 1);
+    ASSERT_EQ (request.host, "localhost");
+    ASSERT_EQ (request.ip, "127.0.0.1");
+    ASSERT_EQ (request.protocol, lightning::ProtocolType::kHttp);
+    ASSERT_TRUE (request.headers.contains("host"));
+    ASSERT_TRUE (request.headers.contains("headername"));
+    ASSERT_EQ (request.body.size(), 0);
+    response.status(200).send ("Hello World!");
+  });
+
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
+
+  const auto exit = std::system (fmt::format(
+    "curl -s -X CONNECT 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -w '%{{http_code}}' -o {} > {}",
+    bodyFileName.string(),
+    statusFileName.string()
+  ).c_str());
+  ASSERT_EQ (exit, 0);
+
+  const auto [ resStatus, resBody ] = readResponse (statusFileName, bodyFileName);
+  ASSERT_EQ (resStatus, 200);
+  ASSERT_EQ (resBody, "Hello World!");
+}
+
+// ----------------------------------------------------------------------------
+// test_options_simple_text
+// ----------------------------------------------------------------------------
+TEST (HttpServer, test_options_simple_text) {
+  lightning::HttpServer server { 8080, getLogLevel() };
+
+  const std::string body { "aa=bb" };
+
+  server.addRoute (lightning::HttpMethod::kOptions, "/v1/hello", [ &body ] (const auto &request, auto &response) {
+    ASSERT_EQ (request.method, lightning::HttpMethod::kOptions);
+    ASSERT_EQ (request.path, "/v1/hello");
+    ASSERT_EQ (request.query, "p=1");
+    ASSERT_EQ (request.url, "/v1/hello?p=1");
+    ASSERT_EQ (request.version.major, 1);
+    ASSERT_EQ (request.version.minor, 1);
+    ASSERT_EQ (request.host, "localhost");
+    ASSERT_EQ (request.ip, "127.0.0.1");
+    ASSERT_EQ (request.protocol, lightning::ProtocolType::kHttp);
+    ASSERT_TRUE (request.headers.contains("host"));
+    ASSERT_TRUE (request.headers.contains("headername"));
+    ASSERT_EQ (request.body.size(), body.size());
+    response.status(200).send ("Hello World!");
+  });
+
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
+
+  const auto exit = std::system (fmt::format(
+    "curl -s -X OPTIONS 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -d '{}' -w '%{{http_code}}' -o {} > {}",
+    body,
+    bodyFileName.string(),
+    statusFileName.string()
+  ).c_str());
+  ASSERT_EQ (exit, 0);
+
+  const auto [ resStatus, resBody ] = readResponse (statusFileName, bodyFileName);
+  ASSERT_EQ (resStatus, 200);
+  ASSERT_EQ (resBody, "Hello World!");
+}
+
+// ----------------------------------------------------------------------------
+// test_trace_simple_text
+// ----------------------------------------------------------------------------
+TEST (HttpServer, test_trace_simple_text) {
+  lightning::HttpServer server { 8080, getLogLevel() };
+
+  const std::string body { "aa=bb" };
+
+  server.addRoute (lightning::HttpMethod::kTrace, "/v1/hello", [ &body ] (const auto &request, auto &response) {
+    ASSERT_EQ (request.method, lightning::HttpMethod::kTrace);
+    ASSERT_EQ (request.path, "/v1/hello");
+    ASSERT_EQ (request.query, "p=1");
+    ASSERT_EQ (request.url, "/v1/hello?p=1");
+    ASSERT_EQ (request.version.major, 1);
+    ASSERT_EQ (request.version.minor, 1);
+    ASSERT_EQ (request.host, "localhost");
+    ASSERT_EQ (request.ip, "127.0.0.1");
+    ASSERT_EQ (request.protocol, lightning::ProtocolType::kHttp);
+    ASSERT_TRUE (request.headers.contains("host"));
+    ASSERT_TRUE (request.headers.contains("headername"));
+    ASSERT_EQ (request.body.size(), body.size());
+    response.status(200).send ("Hello World!");
+  });
+
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
+
+  const auto exit = std::system (fmt::format(
+    "curl -s -X TRACE 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -d '{}' -w '%{{http_code}}' -o {} > {}",
+    body,
+    bodyFileName.string(),
+    statusFileName.string()
+  ).c_str());
+  ASSERT_EQ (exit, 0);
+
+  const auto [ resStatus, resBody ] = readResponse (statusFileName, bodyFileName);
+  ASSERT_EQ (resStatus, 200);
+  ASSERT_EQ (resBody, "Hello World!");
+}
+
+// ----------------------------------------------------------------------------
+// test_patch_simple_text
+// ----------------------------------------------------------------------------
+TEST (HttpServer, test_patch_simple_text) {
+  lightning::HttpServer server { 8080, getLogLevel() };
+
+  const std::string body { "aa=bb" };
+
+  server.addRoute (lightning::HttpMethod::kPatch, "/v1/hello", [ &body ] (const auto &request, auto &response) {
+    ASSERT_EQ (request.method, lightning::HttpMethod::kPatch);
+    ASSERT_EQ (request.path, "/v1/hello");
+    ASSERT_EQ (request.query, "p=1");
+    ASSERT_EQ (request.url, "/v1/hello?p=1");
+    ASSERT_EQ (request.version.major, 1);
+    ASSERT_EQ (request.version.minor, 1);
+    ASSERT_EQ (request.host, "localhost");
+    ASSERT_EQ (request.ip, "127.0.0.1");
+    ASSERT_EQ (request.protocol, lightning::ProtocolType::kHttp);
+    ASSERT_TRUE (request.headers.contains("host"));
+    ASSERT_TRUE (request.headers.contains("headername"));
+    ASSERT_EQ (request.body.size(), body.size());
+    response.status(200).send ("Hello World!");
+  });
+
+  const auto [ statusFileName, bodyFileName ] = createTempFiles();
+
+  const auto exit = std::system (fmt::format(
+    "curl -s -X PATCH 'http://localhost:8080/v1/hello?p=1' -H 'HeaderName: header value' -H 'Content-Type: text/plain' -d '{}' -w '%{{http_code}}' -o {} > {}",
     body,
     bodyFileName.string(),
     statusFileName.string()
